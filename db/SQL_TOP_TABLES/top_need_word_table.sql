bb0cce1bad08c33f4747c6148b5e9dd5614619e7
matisin <matias.medina.s2@gmail.com>
2021-04-07 15:03:46 -0400

    group by dialogue

--- db/SQL_TOP_TABLES/top_need_word_table.sql
+++ db/SQL_TOP_TABLES/top_need_word_table.sql
@@ -17,10 +17,10 @@ FOR temprow IN
     LOOP
         INSERT INTO top_need_role_table
         SELECT pair_roles.palabra_inicio, need.macro 
-        FROM pair_roles, need_pairs, need
+        FROM pair_roles, need_role_pairs, need
         WHERE 
-          pair_roles.pair_id = need_pairs.pair_id AND
-          need_pairs.need_id = need.need_id AND
+          pair_roles.pair_id = need_role_pairs.pair_id AND
+          need_role_pairs.need_id = need.need_id AND
           need.macro = temprow.macro
         GROUP BY pair_roles.palabra_inicio, need.macro
         ORDER BY COUNT(*) DESC LIMIT 10;
@@ -37,10 +37,10 @@ FOR temprow IN
     LOOP
         INSERT INTO top_need_exp_table
         SELECT pair_exp.palabra_inicio, need.macro 
-        FROM pair_exp, need_pairs, need
+        FROM pair_exp, need_exp_pairs, need
         WHERE 
-          pair_exp.pair_id = need_pairs.pair_id AND
-          need_pairs.need_id = need.need_id AND
+          pair_exp.pair_id = need_exp_pairs.pair_id AND
+          need_exp_pairs.need_id = need.need_id AND
           need.macro = temprow.macro
         GROUP BY pair_exp.palabra_inicio, need.macro
         ORDER BY COUNT(*) DESC LIMIT 10;
